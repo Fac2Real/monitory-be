f5f910c65fc8852734f1de9309ddb5d2be882ff8
광부입니다 <kwy1379@ajou.ac.kr>
2025-05-11 02:03:51 +0900

    feat | sprint2 | FRB-54 | 읽지 않은 푸시알림용 서비스 + 컨트롤러 + 웹푸시 구현 | 김우영

---
+++ src/main/java/com/factoreal/backend/dto/abnormalLog/AbnormalPagingDto.java
@@ -0,0 +1,9 @@
+package com.factoreal.backend.dto.abnormalLog;
+
+import lombok.Data;
+
+@Data
+public class AbnormalPagingDto {
+    private int page;
+    private int size;
+}
--- src/main/java/com/factoreal/backend/repository/AbnLogRepository.java
+++ src/main/java/com/factoreal/backend/repository/AbnLogRepository.java
@@ -1,7 +1,15 @@
 package com.factoreal.backend.repository;
 
 import com.factoreal.backend.entity.AbnormalLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AbnLogRepository extends JpaRepository<AbnormalLog,Long> {
+    Page<AbnormalLog> findAbnormalLogsByAbnormalType(String abnormalType, Pageable pageable);
+
+    Page<AbnormalLog> findAbnormalLogsByTargetTypeAndTargetId(String targetType, String targetId, Pageable pageable);
+    long countByIsReadFalse(); // 읽지 않은 로그의 개수 반환
+
+    Page<AbnormalLog> findAllByIsReadIsFalse(Pageable pageable);
 }
